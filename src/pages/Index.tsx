906c0fbdeb86058d4f622c3a674aa6f4bc68182d
Dev Patel954 <dpate440@uic.edu>
2025-12-31 22:35:37 -0600

    Add Resume Tailor frontend with all components

---
+++ src/pages/Index.tsx
@@ -0,0 +1,29 @@
+import Navbar from '@/components/layout/Navbar';
+import Footer from '@/components/layout/Footer';
+import HeroSection from '@/components/landing/HeroSection';
+import HowItWorks from '@/components/landing/HowItWorks';
+import FeaturesSection from '@/components/landing/FeaturesSection';
+import TestimonialsSection from '@/components/landing/TestimonialsSection';
+import PricingSection from '@/components/landing/PricingSection';
+import FAQSection from '@/components/landing/FAQSection';
+import CTASection from '@/components/landing/CTASection';
+
+const Index = () => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-1">
+        <HeroSection />
+        <HowItWorks />
+        <FeaturesSection />
+        <TestimonialsSection />
+        <PricingSection />
+        <FAQSection />
+        <CTASection />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
